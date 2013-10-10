0b3cfedbf45c566d966cc04baa0eadeffa25b3b4
Eric Hare <ericrhare@gmail.com>
2013-10-10 11:44:43 -0500

    Bring tabulate into sync with the new formate

--- R/tabulate.r
+++ R/tabulate.r
@@ -5,7 +5,7 @@
 encodingReduce <- function(class, libscheme) {
     sapply(class, function(x) { 
         splitted <- strsplit(x, split = "")[[1]]
-        lvls <- libscheme(1)$info$scheme$class
+        lvls <- libscheme$info$scheme$class
         myTbl <- table(factor(splitted, levels = lvls[lvls != "Z"]))
         return(paste(myTbl, collapse = ","))
     })
@@ -16,7 +16,7 @@ encodingReduce <- function(class, libscheme) {
 #' @param k Peptide length
 #' @return Character vector of possible counts for each class
 getCounts <- function(libscheme, k) {
-    scheme.levels <- length(libscheme(1)$info$scheme$class) - 1
+    scheme.levels <- length(libscheme$info$scheme$class) - 1
     
     str.func <- paste("expand.grid(", paste(rep(paste("0:", k, sep = ""), times = scheme.levels), collapse = ", "), ")")
     
@@ -45,32 +45,35 @@ getChoices <- function(str) {
 }
 
 #' For a given scheme, generate a dataset with the peptide probabilities
-#' @param Custom function representing the scheme to use
+#' @param scheme_def definition of the custom scheme
 #' @return A data frame of peptide probabilities
-generateCustomProbs <- function(Custom) {
+generateCustomProbs <- function(scheme_def) {
     ## The Constants
     k <- 6:10
     N <- c(10^6, 10^7, 10^8, 10^9, 10^10, 10^11, 10^12)
     n <- as.vector(sapply(10^seq(6, 12, by = 1), `*`, seq(1.0, 9.9, by = 0.1)))
     
+    ## Generate scheme
+    lib <- libscheme(scheme_def)
+    
     cat("Getting possible peptide encodings...\n")
     lib.probs.tmp <- ldply(k, function(y) {
-        df <- data.frame(Counts = getCounts(Custom, y))
+        df <- data.frame(Counts = getCounts(lib, y))
         df$k <- y
         
         df
     })
     
     cat("Getting a sample peptide encoding...\n")
-    lib.probs.tmp$samp.encoding <- apply(lib.probs.tmp, 1, function(z) { paste(rep(Custom(1)$info$scheme$class, as.numeric(c(strsplit(as.character(z), split = ",")[[1]], 0))), collapse = "") })
+    lib.probs.tmp$samp.encoding <- apply(lib.probs.tmp, 1, function(z) { paste(rep(lib$info$scheme$class, as.numeric(c(strsplit(as.character(z), split = ",")[[1]], 0))), collapse = "") })
     
     cat("Processing probabilities...\n")
     lib.probs <- ldply(k, .progress = "text", .fun = function(y) {
-        lib.data <- get("Custom")(y)$data
+        lib.data <- libscheme(scheme_def, y)$data
         lib.data$class <- gsub("\\.", "", lib.data$class)
         lib.data.subset <- subset(lib.data, class %in% lib.probs.tmp$samp.encoding)
         
-        return.df <- data.frame(Counts = encodingReduce(as.character(lib.data.subset$class), Custom), samp.encoding = lib.data.subset$class, probs = lib.data.subset$probs)
+        return.df <- data.frame(Counts = encodingReduce(as.character(lib.data.subset$class), lib), samp.encoding = lib.data.subset$class, probs = lib.data.subset$probs)
         #return.df$probs <- apply(return.df, 1, function(z) {as.numeric(z[3]) / prod(getProbCorrection(z[3], x))})
         return.df$k <- y
         return.df$di <- lib.data.subset$di
@@ -83,9 +86,9 @@ generateCustomProbs <- function(Custom) {
 }
 
 #' For a given scheme, generate a dataset with the library information
-#' @param Custom function representing the scheme to use
+#' @param scheme_def definition of the custom scheme
 #' @return A data frame of library information
-generateCustomLib <- function(Custom) {
+generateCustomLib <- function(scheme_def) {
     ## The Constants
     k <- 6:10
     N <- c(10^6, 10^7, 10^8, 10^9, 10^10, 10^11, 10^12)
@@ -95,19 +98,19 @@ generateCustomLib <- function(Custom) {
     lib.stats <- ldply(k, function(k1) {
         cat("Processing for k =", k1, "\n")
         
-        lib <- Custom(k1)
+        lib <- libscheme(scheme_def, k = k1)
         n <- as.vector(sapply(10^seq(6, 12, by = 1), `*`, seq(1.0, 9.9, by = 0.1)))
         
         lib.stats <- ldply(n, .progress = "text", function(n1) {  
             # cat("Processing for n =", n1, "\n")
             
-            cov = coverage(k=k1, libscheme=Custom, N=n1, lib=lib)
-            eff = efficiency(k=k1, libscheme=Custom, N=n1, lib=lib)
+            cov = coverage(k=k1, libscheme=scheme_def, N=n1, lib=lib)
+            eff = efficiency(k=k1, libscheme=scheme_def, N=n1, lib=lib)
             c(k=k1, n=n1, cov=cov, eff=eff)
         })
         
         cat("Generating library diversity...\n")
-        lib.stats$div = makowski(k=k1, libscheme=Custom)
+        lib.stats$div = makowski(k=k1, libscheme=scheme_def)
         
         lib.stats
     })
@@ -123,16 +126,11 @@ generateCustomLib <- function(Custom) {
 #' @examples
 #' \dontrun{
 #' generateCustom()
-#' generateCustom(scheme_name = "NNK_S", scheme_def = peptider:::nnk_scheme)
+#' generateCustom(scheme_name = "NNN", scheme_def = scheme("NNN"))
 #' }
 generateCustom <- function(scheme_name = "Custom", scheme_def = read.csv(file.choose())) {
-    ## Load the scheme
-    Custom <- function(k) {
-        libBuild(k, scheme_def)
-    }
-    
-    custom.probs <- generateCustomProbs(Custom)
-    custom.lib <- generateCustomLib(Custom)
+    custom.probs <- generateCustomProbs(scheme_def)
+    custom.lib <- generateCustomLib(scheme_def)
     
     write.csv(custom.probs, paste("prob-", scheme_name, ".csv", sep = ""), row.names = FALSE)
     write.csv(custom.lib, paste("lib-", scheme_name, ".csv", sep = ""), row.names = FALSE)
