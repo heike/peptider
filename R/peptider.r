0d421fe97e470d991f172d6f3c2e9891829507f1
Heike Hofmann <hofmann@iastate.edu>
2014-07-01 16:39:00 -0500

    added reduced version of genNeighbors

--- R/peptider.r
+++ R/peptider.r
@@ -457,7 +457,7 @@ NULL
 #' @export
 #' @examples
 #' genNeighbors(scheme("NNK"), 2)
-#' genNeighbors("Trimer", 2)
+#' genNeighbors(scheme("Trimer"), 2)
 genNeighbors <- function(sch, k) {
 #    sch <- libscheme(lib,1)$info$scheme
     
@@ -508,4 +508,83 @@ genNeighbors <- function(sch, k) {
     ## number of neighbors
     x$N1 <- with(x, c0*(cr+1))
     x
+}
+
+#' Calculate neighborhood distribution
+#' 
+#' Calculate distribution of neighbors under library scheme lib for peptide sequences of length k.
+#' @param sch library scheme
+#' @param k length of the peptide sequences
+#' @return dataset of peptide sequences: L are amino acid sequences, 
+#' c0 are codons for self representation, 
+#' cr is the ratio of #neighbors in first degree neighborhood (not counting self representations) and #codons in self representation
+#' N1 is the number of neighbors in codon representation (including self representation) 
+#' s is the number of peptide sequences described by the label
+#' o is the number of peptide sequences reached by permutations
+#' @export
+#' @examples
+#' genNeighbors_reduced(scheme("NNK"), 2)
+#' genNeighbors_reduced(scheme("Trimer"), 2)
+genNeighbors_reduced <- function(sch, k) {    
+    schl <- unlist(strsplit(as.character(sch$aacid[-length(sch$aacid)]), ""))
+    schd <- data.frame(AA=schl, C0=codons(schl, lib=lib))
+    schd$C1 <- getNofNeighbors(schd$AA, method="codon", lib=lib) - schd$C0
+    schd$C1T0 <- with(schd, C1/C0)
+    
+    ctabs <- function(values, labels) {
+        labels <- as.character(labels)
+        
+        tv <- xtabs(~values)
+        require(plyr)
+        ct <- ldply(names(tv), function(x) {
+            data.frame(AA=paste(labels[which(values == x)], collapse=""),
+                       c=x)
+        })
+        ct
+    }
+    
+    user <- with(schd, ctabs(paste(C0, C1, sep=":"), AA))
+    user <- data.frame(user, ldply(strsplit(as.character(user$c), ":"), 
+                                   function(x) as.numeric(unlist(x))))
+    names(user)[3:4] <- c("c0", "c1")
+    user$cr <- with(user, c1/c0)
+    user$c <- user$c0
+    user$s <- nchar(as.character(user$AA))
+    user$one <- 1
+    
+    i <- 1
+    dx <- user
+    dx$o <- dx$one
+    dx$L <- user$AA
+    while (i < k) {
+        C0 <- as.vector(outer(dx$c0, user$c, FUN = "*"))
+        S <- as.vector(outer(dx$s, user$s, FUN = "*"))
+        CR <- as.vector(outer(dx$cr, user$cr, FUN = "+"))
+        O <- as.vector(outer(dx$o, user$one, FUN = "*"))
+        #    L <- as.vector(outer(L, user$AA, FUN = function(x,y) {        
+        #        x <- as.character(x)
+        #        y <- as.character(y)
+        #       paste(pmin(x,y), pmax(x,y), sep=",") 
+        #    }))    
+        L <- as.vector(outer(dx$L, user$AA, "paste", sep = ","))
+        lx <- strsplit(as.character(L), split=",")
+        L <- laply(llply(lx, sort), paste, collapse=",")
+        
+        x <- data.frame(L, C0,CR,S,O)
+        dx <- ddply(x, .(L), summarise, 
+                    c0=C0[1],
+                    cr=CR[1],
+                    s=S[1],
+                    o=sum(O))
+        
+        #    save(L,C0,CR,S,O, file=sprintf("%s-%d.RData", lib, i))
+        dx$N1 <- with(dx, c0*(cr+1))
+        dx$lib <- lib
+        dx$k <- i+1
+        #   cat(sprintf("stage %d done\n",i))
+        #   write.table(dx, file="neighbors2.csv", sep=",", col.names=!file.exists("neighbors2.csv"), append=TRUE, row.names=FALSE)
+        
+        i <- i + 1
+    }
+    dx
 }
